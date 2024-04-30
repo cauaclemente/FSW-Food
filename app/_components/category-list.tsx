0c3dd11d89a5b165c9ac7a2c72162d5609d96c2c
Cauã Clemente <cauaclemente07@gmail.com>
2024-04-30 20:40:37 -0300

    feat: add promo banner

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -1,5 +1,18 @@
-const CategoryList = () => {
-  return <></>;
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-3">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default CategoryList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
 
@@ -7,6 +9,22 @@ const Home = () => {
       <Header />
       <div className="px-5 pt-6">
         <Search />
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+
+      <div className="px-5 py-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Promoção de ate 30% em pizzas!"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
