2d3cefaac23852c523ec48ec54491efed49ef797
Cauã Clemente <cauaclemente07@gmail.com>
2024-05-01 18:39:02 -0300

    feat: add restaurant list component

---
+++ app/_components/restaurant-list.tsx
@@ -0,0 +1,18 @@
+import { db } from "../_lib/prisma";
+import RestaurantItem from "./restaurant-item";
+
+const RestaurantList = async () => {
+  const restaurants = await db.restaurant.findMany({ take: 10 });
+
+  return (
+    <>
+      <div className="flex gap-4 overflow-y-scroll px-5 [&::-webkit-scrollbar]:hidden">
+        {restaurants.map((restaurant) => (
+          <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default RestaurantList;
