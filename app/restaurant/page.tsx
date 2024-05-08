17f72437c718e9f2cbd5bdf6b2ec1f5597887840
Cauã Clemente <cauaclemente07@gmail.com>
2024-05-08 20:58:39 -0300

    feat: add search for restaurants page

---
+++ app/restaurant/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Restaurant } from "@prisma/client";
+import { notFound, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { searchForRestaurants } from "./search";
+import Header from "../_components/header";
+import RestaurantItem from "../_components/restaurant-item";
+
+const Restaurants = () => {
+  const searchParams = useSearchParams();
+  const [restaurant, setRestaurant] = useState<Restaurant[]>([]);
+
+  const searchFor = searchParams.get("search");
+
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      const searchFor = searchParams.get("search");
+      if (!searchFor) return;
+      const foundRestaurants = await searchForRestaurants(searchFor);
+      setRestaurant(foundRestaurants);
+    };
+
+    fetchRestaurants();
+  }, [searchParams]);
+
+  if (!searchFor) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className=" mb-6 text-lg font-semibold">
+          {" "}
+          Restaurantes Encontrados
+        </h2>
+        <div className="flex w-full flex-col gap-6 md:grid md:grid-cols-4">
+          {restaurant.map((restaurants) => (
+            <RestaurantItem
+              key={restaurants.id}
+              restaurant={restaurants}
+              className="min-w-full max-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Restaurants;
---
+++ app/restaurant/search.ts
@@ -0,0 +1,17 @@
+"use server"
+
+import { db } from "../_lib/prisma"
+
+export const searchForRestaurants =async (search: string) => {
+  const restaurants = await db.restaurant.findMany({
+    where:{
+      name:{
+        contains: search,
+        mode: "insensitive"
+      }
+    }
+  })
+
+  return restaurants;
+}
+
