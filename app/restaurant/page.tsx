4d6d0bb03b28ac8d2ddc82488c4577ecbc4ccb82
Cauã Clemente <cauaclemente07@gmail.com>
2024-05-23 18:13:22 -0300

    feat: move restaurant list to a client component

--- app/restaurant/page.tsx
+++ app/restaurant/page.tsx
@@ -1,53 +1,12 @@
-"use client";
+import { Suspense } from "react";
+import Restaurants from "./restaurant";
 
-import { Restaurant } from "@prisma/client";
-import { notFound, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { searchForRestaurants } from "./search";
-import Header from "../_components/header";
-import RestaurantItem from "../_components/restaurant-item";
-
-const Restaurants = () => {
-  const searchParams = useSearchParams();
-  const [restaurant, setRestaurant] = useState<Restaurant[]>([]);
-
-  const searchFor = searchParams.get("search");
-
-  useEffect(() => {
-    const fetchRestaurants = async () => {
-      const searchFor = searchParams.get("search");
-      if (!searchFor) return;
-      const foundRestaurants = await searchForRestaurants(searchFor);
-      setRestaurant(foundRestaurants);
-    };
-
-    fetchRestaurants();
-  }, [searchParams]);
-
-  if (!searchFor) {
-    return notFound();
-  }
-
+const RestaurantsPage = () => {
   return (
-    <>
-      <Header />
-      <div className="px-5 py-6">
-        <h2 className=" mb-6 text-lg font-semibold">
-          {" "}
-          Restaurantes Encontrados
-        </h2>
-        <div className="flex w-full flex-col gap-6 md:grid md:grid-cols-4">
-          {restaurant.map((restaurants) => (
-            <RestaurantItem
-              key={restaurants.id}
-              restaurant={restaurants}
-              className="min-w-full max-w-full"
-            />
-          ))}
-        </div>
-      </div>
-    </>
+    <Suspense>
+      <Restaurants />
+    </Suspense>
   );
 };
 
-export default Restaurants;
+export default RestaurantsPage;
